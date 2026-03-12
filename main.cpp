24f7091fdc22eaacac5143dbc7d385f15130495a
alfa98516 <alpha050916@gmail.com>
2026-03-12 15:24:12 +0000

    UPDATE :: started work on the second draw function

--- main.cpp
+++ main.cpp
@@ -1,15 +1,18 @@
 #include "enemy.hpp"
 #include "window.hpp"
+#include <fstream>
 #include <iostream>
 #include <string>
 int main() {
     Window w = Window();
-    std::string a = " aaa \n\
-aaaaa\n\
-aaaaa\n\
- aaa ";
-    int option;
+    std::string a = "";
+    std::ifstream f("frogtest.txt");
+    std::string s;
+    while (getline(f, s))
+        a += s + '\n';
+    f.close();
     std::cout << "Select: ";
+    int option;
     std::cin >> option;
     w.clearScreen();
     w.draw(a);
--- window.hpp
+++ window.hpp
@@ -1,6 +1,7 @@
 #include "enemy.hpp"
 #include <cstddef>
 #include <iostream>
+#include <memory>
 #include <string>
 
 #ifndef WINDOW
@@ -51,14 +52,15 @@ class Window {
     void draw(std::string element) {
         std::cout
             << "╔══════════════════════════════════════════════════════════════════════════════╗\n";
-        int w = width(element);
+        std::shared_ptr<std::vector<int> > temp = width(element);
+        std::vector<int> w = (temp == nullptr) ? std::vector<int>{-1} : *temp.get();
         int h = height(element);
-        if (w == -1 || h == -1) // element doesn't fit
+        if (w[0] == -1 || h == -1) // element doesn't fit
             return;
         int hCount = 0;
         int wCount = 0;
-        const int wPlacementIdx =
-            (WIDTH - w) / 2; // where should we start printing on the current line?
+        int wPlacementIdx =
+            (WIDTH - w[0]) / 2; // where should we start printing on the current line?
         const int hPlacementIdx = (HEIGHT - h) / 2; // on what line can we start printing?
 
         // add padding for the element.
@@ -69,46 +71,54 @@ class Window {
             hCount++;
         }
         char c;
+        int j = 0;
         int i = 0;
-        for (int i = 0; i < element.length();) { // I ommit the increment here because we
-                                                 // dont always want to increment.
+        int wPlacementCount = 0;
+        for (int i = 0; i < element.length();) {
             c = element[i];
-
-            if (c == '\n') { // Here I'm adding the trailing spaces behind the the element.
-                int j = 0;
-
-                while (j <
-                       wPlacementIdx - 1) { // wPlacementIdx coincidentaly works here because its
-                    // technically just how much padding to add to the element
+            wPlacementIdx = (WIDTH - w[j]) / 2;
+            if (c == '\n') {
+                while (wPlacementCount <
+                       WIDTH - 2) { // removes the two ║ characters out of the count.
                     std::cout << " ";
-                    j++;
+                    wPlacementCount++;
                 }
-                std::cout << "║\n";
+                wPlacementCount = 0;
                 wCount = 0;
                 c = element[++i];
+                j++;
+                std::cout << "║\n";
             }
 
-            if (wCount == 0) // start of the line
+            if (wCount == 0 && c != '\n') {
                 std::cout << "║";
+            }
 
-            while (wCount < wPlacementIdx) { // add padding until we can print a character
+            while (wCount < wPlacementIdx && c != '\n') {
                 std::cout << " ";
                 wCount++;
+                wPlacementCount++;
             }
-            if (wCount >= wPlacementIdx) {
+            if (c != '\n') {
                 std::cout << c;
-                wCount++;
+                wPlacementCount++;
                 i++;
             }
         }
 
-        int j = 0;
-        while (j < wPlacementIdx - 1) { // when on the last character, it wont add trailing spaces,
-                                        // since the for loop ends before it can attempt.
-
+        while (wPlacementCount < WIDTH - 1) { // I have no idea why - 1 works here but it does and
+                                              // im not clambering to figure out why.
             std::cout << " ";
-            j++;
+            wPlacementCount++;
         }
+
+        // int k = 0;
+        // while (k < wPlacementIdx - 1) { // when on the last character, it wont add trailing
+        // spaces,
+        //                                 // since the for loop ends before it can attempt.
+        //     std::cout << " ";
+        //     k++;
+        // }
 
         std::cout << "║\n";
         int count = 0;
@@ -126,7 +136,59 @@ class Window {
                      "══════╝\n";
     }
 
-    void draw(std::string element1, std::string element2) {}
+    void draw(std::string element1, std::string element2) {
+        std::shared_ptr<std::vector<int> > temp1 = width(element1);
+        std::vector<int> w1 = (temp1 == nullptr) ? std::vector<int>{-1} : *temp1.get();
+        int h1 = height(element1);
+        if (w1[0] == -1 || h1 == -1) // element doesn't fit
+            return;
+        int h1Count = 0;
+        int w1Count = 0;
+        std::shared_ptr<std::vector<int> > temp2 = width(element2);
+        std::vector<int> w2 = (temp2 == nullptr) ? std::vector<int>{-1} : *temp2.get();
+        int h2 = height(element2);
+        if (w2[0] == -1 || h2 == -1) // element doesn't fit
+            return;
+        int h2Count = 0;
+        int w2Count = 0;
+
+        const int h1PlacementIndex = (HEIGHT - h1) / 2;
+        const int h2PlacementIndex = (HEIGHT - h2) / 2;
+
+        std::vector<std::vector<std::string> > wind =
+            std::vector<std::vector<std::string> >(20, std::vector<std::string>(80));
+
+        // fill window in
+        for (int i = 0; i < 20; ++i) {
+            for (int j = 0; j < 80; ++j) {
+                if ((i == 0 || i == 19) && ((j > 0 && j < 79) || (j > 0 && j < 79))) {
+                    wind[i][j] = "═";
+                } else if (i == 0 && j == 0) {
+                    wind[0][0] = "╔";
+                } else if (i == 0 && j == 79) {
+                    wind[i][j] = "╗";
+                } else if (i == 19 && j == 0) {
+                    wind[i][j] = "╚";
+                } else if (i == 19 && j == 79) {
+                    wind[i][j] = "╝";
+                } else if ((i != 0 && i != 79) && (j == 0 || j == 79)) {
+                    wind[i][j] = "║";
+                } else {
+                    wind[i][j] = " ";
+                }
+            }
+
+            for (int i = 0; i < element1.size();) {
+            }
+
+            for (std::vector<std::string> v : wind) {
+                for (std::string c : v) {
+                    std::cout << c;
+                }
+                std::cout << '\n';
+            }
+        }
+    }
     void draw(std::string element1, std::string element2, std::string element3) {}
 
   protected:
@@ -144,21 +206,26 @@ class Window {
      * @param element: The string we want to measure.
      * @return int: The width of the element, if the element does not fit, it returns -1.
      */
-    int width(std::string element) {
-        int w = 0;
+    std::shared_ptr<std::vector<int> > width(std::string element) {
+        int currW = 0;
+        int maxW = 0;
+        std::shared_ptr<std::vector<int> > w = std::make_shared<std::vector<int> >();
         for (char c : element) {
-            if (c == '\n')
-                return w;
-            w++;
+            if (c == '\n') {
+                w->push_back(currW);
+                if (currW > maxW) maxW = currW;
+                currW = 0;
+            }
+            currW++;
         }
-        return (w <= WIDTH) ? w : -1;
+
+        return (maxW <= WIDTH) ? w : nullptr;
     }
 
     int height(std::string element) {
         int h = 0;
         for (char c : element) {
-            if (c == '\n')
-                h++;
+            if (c == '\n') h++;
         }
         return (h + 1 <= HEIGHT) ? h + 1 : -1;
     }
